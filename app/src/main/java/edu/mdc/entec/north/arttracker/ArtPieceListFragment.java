7891abd5090bb1ae72583aa64f927deaedb2c758
Entec01 <alinamehdi15@gmail.com>
2018-03-07 11:40:49 -0500

    initial

---
+++ app/src/main/java/edu/mdc/entec/north/arttracker/ArtPieceListFragment.java
@@ -0,0 +1,90 @@
+package edu.mdc.entec.north.arttracker;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ArtPieceListFragment extends Fragment {
+    private static final String ART_PIECES = "artPieces";
+    private static final String TAG = "ArtPieceListFragment";
+    private List<ArtPiece> artPieces;
+    private ArtPieceAdapter adapter;
+    private RecyclerView recyclerView;
+    private OnArtPieceSelectedListener mListener;
+
+    public ArtPieceListFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static ArtPieceListFragment newInstance(List<ArtPiece> artPieces) {
+        ArtPieceListFragment fragment = new ArtPieceListFragment();
+        Bundle args = new Bundle();
+        args.putParcelableArrayList(ART_PIECES, (ArrayList<? extends Parcelable>) artPieces);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            artPieces = getArguments().getParcelableArrayList(ART_PIECES);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_art_piece_list, container, false);
+
+
+        //Lookup the recyclerview
+        recyclerView = (RecyclerView) view.findViewById(R.id.rv_art_pieces);
+        // Create adapter passing in the data
+        adapter = new ArtPieceAdapter(artPieces, mListener);
+        // Attach the adapter to the recyclerview to populate items
+        recyclerView.setAdapter(adapter);
+        // Set layout manager to position the items
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Fragment galleryFragment = this.getParentFragment();
+        if (galleryFragment instanceof OnArtPieceSelectedListener) {
+            mListener = (OnArtPieceSelectedListener) galleryFragment;
+        } else {
+            Log.d(TAG, "The listener must be implemented in GalleryFragment");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+    public interface OnArtPieceSelectedListener {
+        void onArtPieceSelected(int position);
+    }
+
+
+
+}
